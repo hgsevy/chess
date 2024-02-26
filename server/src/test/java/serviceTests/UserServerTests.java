97d1848fb4420aaadb2a7f982f5438c4b3a73802
hgsevy <hgsevy@gmail.com>
2024-02-25 17:30:47 -0700

    Created user service unit tests

---
+++ server/src/test/java/serviceTests/UserServerTests.java
@@ -0,0 +1,111 @@
+package serviceTests;
+
+import dataAccess.MemoryAuthDAO;
+import dataAccess.MemoryUserDAO;
+import dataAccess.UserDAO;
+import model.UserData;
+import org.junit.jupiter.api.*;
+import service.LoginRequest;
+import service.LoginResult;
+import service.UserService;
+import service.exceptions.BadInputException;
+import service.exceptions.NoCanDoException;
+import service.exceptions.UnauthorizedException;
+
+public class UserServerTests {
+
+    private UserService service;
+    private MemoryAuthDAO authData;
+    private UserDAO userData;
+    @BeforeEach
+    public void createObjects(){
+        authData = new MemoryAuthDAO();
+        userData = new MemoryUserDAO();
+        service = new UserService(userData, authData);
+    }
+
+    @Test
+    @DisplayName("Successful Registration")
+    public void goodRegister() throws NoCanDoException, BadInputException {
+        String username = "johndoe";
+        String password = "12345";
+        String email = "jdoe@yahoo.com";
+        LoginResult result = service.register(new UserData(username, password, email));
+        Assertions.assertEquals(username, result.username());
+        Assertions.assertNotNull(result.authToken());
+    }
+
+    @Test
+    @DisplayName("Double Registration")
+    public void dublicateRegister() throws NoCanDoException, BadInputException {
+        String username = "johndoe";
+        String password = "12345";
+        String email = "jdoe@yahoo.com";
+        UserData data = new UserData(username, password, email);
+        service.register(data);
+        Assertions.assertThrows(NoCanDoException.class, ()->service.register(data));
+    }
+
+    @Test
+    @DisplayName("Incomplete Registration")
+    public void incompleteRegister() throws NoCanDoException, BadInputException {
+        String username = "johndoe";
+        String password = "12345";
+        UserData data = new UserData(username, password, null);
+        Assertions.assertThrows(BadInputException.class, ()->service.register(data));
+    }
+
+    @Test
+    @DisplayName("Good Login")
+    public void goodLogin() throws NoCanDoException, BadInputException, UnauthorizedException {
+        String username = "johndoe";
+        String password = "12345";
+        String email = "jdoe@yahoo.com";
+        UserData data = new UserData(username, password, email);
+        service.register(data);
+
+        LoginResult result = service.login(new LoginRequest(username, password));
+        Assertions.assertEquals(username, result.username());
+        Assertions.assertNotNull(result.authToken());
+
+        Assertions.assertDoesNotThrow(()->authData.getAuth(result.authToken()));
+    }
+
+    @Test
+    @DisplayName("Bad Login")
+    public void badLogin() throws NoCanDoException, BadInputException, UnauthorizedException {
+        String username = "johndoe";
+        String password = "12345";
+        String email = "jdoe@yahoo.com";
+        UserData data = new UserData(username, password, email);
+        service.register(data);
+
+        Assertions.assertThrows(UnauthorizedException.class, ()->service.login(new LoginRequest(username, "54321")));
+    }
+
+    @Test
+    @DisplayName("Good Logout")
+    public void logout() throws NoCanDoException, BadInputException, UnauthorizedException {
+        String username = "johndoe";
+        String password = "12345";
+        String email = "jdoe@yahoo.com";
+        UserData data = new UserData(username, password, email);
+        LoginResult result = service.register(data);
+        // make sure auth token exists
+        Assertions.assertDoesNotThrow(()->service.logout(result.authToken()));
+        // make sure token got deleted
+        Assertions.assertThrows(UnauthorizedException.class, ()->service.logout(result.authToken()));
+    }
+
+    @Test
+    @DisplayName("Bad Logout")
+    public void BadLogout() throws NoCanDoException, BadInputException, UnauthorizedException {
+        String username = "johndoe";
+        String password = "12345";
+        String email = "jdoe@yahoo.com";
+        UserData data = new UserData(username, password, email);
+        LoginResult result = service.register(data);
+
+        Assertions.assertThrows(UnauthorizedException.class, ()->service.logout(username));
+    }
+}
