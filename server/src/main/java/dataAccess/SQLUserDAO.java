68b5d93398f474134885f50662009a02f002f17a
hgsevy <hgsevy@gmail.com>
2024-03-07 10:43:48 -0700

    add User

--- server/src/main/java/dataAccess/SQLUserDAO.java
+++ server/src/main/java/dataAccess/SQLUserDAO.java
@@ -2,6 +2,7 @@ package dataAccess;
 
 import model.UserData;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static dataAccess.DatabaseManager.getConnection;
@@ -46,7 +47,26 @@ public class SQLUserDAO implements UserDAO{
 
     @Override
     public void createUser(UserData user) throws DataAccessException {
-
+        try (var conn = getConnection()) {
+            var checkTable = "SELECT username FROM userData WHERE username=? OR email=?";
+            try (var selectTableStatement = conn.prepareStatement(checkTable)) {
+                selectTableStatement.setString(1, user.username());
+                selectTableStatement.setString(2, user.email());
+                ResultSet rs = selectTableStatement.executeQuery();
+                if(rs.next()){
+                    throw new DataAccessException("email or username already used by another user");
+                }
+            }
+            var addUser = "INSERT INTO userData(username, password, email) VALUE (?, ?, ?)";
+            try (var insertTableStatement = conn.prepareStatement(addUser)) {
+                insertTableStatement.setString(1, user.username());
+                insertTableStatement.setString(2, user.password());
+                insertTableStatement.setString(3, user.email());
+                insertTableStatement.execute();
+            }
+        } catch (SQLException | DataAccessException e) {
+            System.out.print("womp womp: " + e.getMessage());
+        }
     }
 
     @Override
