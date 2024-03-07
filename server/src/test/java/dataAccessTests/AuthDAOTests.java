7538ac5da7f6572b722257590fbc88550dec97f1
hgsevy <hgsevy@gmail.com>
2024-03-07 16:20:01 -0700

    auth tests but better

--- server/src/test/java/dataAccessTests/AuthDAOTests.java
+++ server/src/test/java/dataAccessTests/AuthDAOTests.java
@@ -3,55 +3,59 @@ package dataAccessTests;
 import dataAccess.AuthDAO;
 import dataAccess.DataAccessException;
 import dataAccess.SQLAuthDAO;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class AuthDAOTests {
+
+    static AuthDAO database;
+
+    @BeforeAll
+    public static void init() throws DataAccessException {
+        database = new SQLAuthDAO();
+    }
     @Test
     public void createAuthGood() throws DataAccessException {
-        AuthDAO database = new SQLAuthDAO();
         String token = database.createAuthToken("username");
         Assertions.assertNotNull(token);
     }
 
     @Test
     public void createAuthBad() throws DataAccessException {
-        AuthDAO database = new SQLAuthDAO();
         Assertions.assertEquals("-1", database.createAuthToken(null));
     }
 
     @Test
     public void getUsernameGood() throws DataAccessException {
-        AuthDAO database = new SQLAuthDAO();
         String token = database.createAuthToken("username");
         Assertions.assertEquals("username", database.getUsername(token));
     }
 
     @Test
     public void getUsernameBad() throws DataAccessException {
-        AuthDAO database = new SQLAuthDAO();
         Assertions.assertThrows(DataAccessException.class, ()->database.getUsername("wrong"));
     }
 
     @Test
     public void deleteAuthGood() throws DataAccessException {
-        AuthDAO database = new SQLAuthDAO();
         String token = database.createAuthToken("username");
         Assertions.assertDoesNotThrow(()->database.deleteAuthToken(token));
     }
 
     @Test
     public void deleteAuthBad() throws DataAccessException {
-        AuthDAO database = new SQLAuthDAO();
         Assertions.assertThrows(DataAccessException.class, ()->database.deleteAuthToken("wrong"));
     }
 
     @Test
     public void clearTest() throws DataAccessException {
-        AuthDAO database = new SQLAuthDAO();
         String token = database.createAuthToken("username");
         Assertions.assertDoesNotThrow(()->database.getUsername(token));
         database.clear();
         Assertions.assertThrows(DataAccessException.class, ()->database.getUsername(token));
+    }
+
+    @AfterEach
+    public void cleanSlate(){
+        database.clear();
     }
 }
