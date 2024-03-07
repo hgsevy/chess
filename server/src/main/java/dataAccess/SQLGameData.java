034c3b22be4dec1662ab44ff88d1b3f71d40a46c
hgsevy <hgsevy@gmail.com>
2024-03-07 11:20:19 -0700

    create game

--- server/src/main/java/dataAccess/SQLGameData.java
+++ server/src/main/java/dataAccess/SQLGameData.java
@@ -1,8 +1,10 @@
 package dataAccess;
 
 import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -16,8 +18,8 @@ public class SQLGameData implements GameDAO{
             var createGameTable = """
             CREATE TABLE  IF NOT EXISTS gameData (
                 gameID INT NOT NULL AUTO_INCREMENT,
-                whiteUsername varchar(255) NOT NULL,
-                blackUsername varchar(255) NOT NULL,
+                whiteUsername varchar(255),
+                blackUsername varchar(255),
                 gameName varchar(255) NOT NULL,
                 game varchar(255) NOT NULL
             )
@@ -32,7 +34,24 @@ public class SQLGameData implements GameDAO{
     }
 
     public int createGame(String name) {
-        return 0;
+        try (var conn = getConnection()) {
+            var addGame = "INSERT INTO gameData(gameName, game) VALUE (?, ?)";
+            try (var insertTableStatement = conn.prepareStatement(addGame)) {
+                insertTableStatement.setString(1, name);
+                insertTableStatement.setString(2, new Gson().toJson(new ChessGame()));
+                insertTableStatement.execute();
+            }
+            var findGame = "SELECT gameID from gameData WHERE gameName=?";
+            try (var insertTableStatement = conn.prepareStatement(addGame)) {
+                insertTableStatement.setString(1, name);
+                ResultSet rs = insertTableStatement.executeQuery();
+                return rs.getInt(1);
+            }
+        } catch (DataAccessException | SQLException e1) {
+            System.out.print("womp womp: " + e1.getMessage());
+        }
+        System.out.print("womp womp: somehow got to SQLGameData line 53");
+        return -1;
     }
 
     public void joinGame(String username, int gameID, ChessGame.TeamColor color) throws DataAccessException {
@@ -44,6 +63,13 @@ public class SQLGameData implements GameDAO{
     }
 
     public void clear() {
-
+        try (var conn = getConnection()) {
+            String command = "TRUNCATE gameData";
+            try (var clearTableStatement = conn.prepareStatement(command)) {
+                clearTableStatement.execute();
+            }
+        } catch (SQLException | DataAccessException e) {
+            System.out.print("womp womp: " + e.getMessage());
+        }
     }
 }
