e09ebdea59d8bf3fc1a02306ed5398958fdf1131
hgsevy <hgsevy@gmail.com>
2024-03-07 12:23:51 -0700

    joingame

--- server/src/main/java/dataAccess/SQLGameData.java
+++ server/src/main/java/dataAccess/SQLGameData.java
@@ -55,7 +55,55 @@ public class SQLGameData implements GameDAO{
     }
 
     public void joinGame(String username, int gameID, ChessGame.TeamColor color) throws DataAccessException {
+        try (var conn = getConnection()) {
+            var checkTable = "SELECT blackUsername, whiteUsername FROM gameData WHERE gameID=?";
+            try (var selectTableStatement = conn.prepareStatement(checkTable)) {
+                selectTableStatement.setInt(1, gameID);
+                ResultSet rs = selectTableStatement.executeQuery();
+                // make sure table exists
+                if(!rs.next()){
+                    throw new DataAccessException("game does not exist");
+                }
+                // choose which spot to join
+                if (color == ChessGame.TeamColor.BLACK){
+                    if (rs.getString(1) == null){
+                        var updateUsers = "UPDATE gameData SET blackUsername=? WHERE id = ?;";
+                        try (var joinStatement = conn.prepareStatement(updateUsers)) {
+                            joinStatement.setString(1, username);
+                            joinStatement.setInt(2, gameID);
+                            joinStatement.execute();
+                        }
+                    }
+                    else if (!rs.getString(1).equals(username)){
+                        throw new DataAccessException("spot already taken");
+                    }
+                }
+                else if (color == ChessGame.TeamColor.WHITE){
+                    if (rs.getString(2) == null){
+                        var updateUsers = "UPDATE gameData SET whiteUsername=? WHERE id = ?;";
+                        try (var joinStatement = conn.prepareStatement(updateUsers)) {
+                            joinStatement.setString(1, username);
+                            joinStatement.setInt(2, gameID);
+                            joinStatement.execute();
+                        }
+                    }
+                    else if (!rs.getString(2).equals(username)){
+                        throw new DataAccessException("spot already taken");
+                    }
+                }
+                else {
+                    throw new DataAccessException("color does not exist");
+                }
 
+            }
+        } catch (DataAccessException e1) {
+            if (e1.getMessage().contains("not exist") || e1.getMessage().contains("taken")){
+                throw e1;
+            }
+            System.out.print(" DataAccess womp womp: " + e1.getMessage());
+        } catch (SQLException e2){
+            System.out.print("SQL womp womp: " + e2.getMessage());
+        }
     }
 
     public ArrayList<GameData> listGames() {
